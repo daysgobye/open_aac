9de0b4e7d62e83090f64bce8e02ececcc6b14389
Cole Smith <smithcole@protonmail.com>
2024-11-05 00:01:31 +0000

    a asart

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import PWABadge from "@/components/common/PWABadge/PWABadge"
+
+
+function App() {
+
+  return (
+    <>
+
+      <PWABadge />
+    </>
+  )
+}
+
+export default App
---
+++ app/vite.config.ts
@@ -0,0 +1,45 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "open aac",
+        short_name: "aac",
+        description: "open free aac comuacation app",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
